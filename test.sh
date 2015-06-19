b7abeb0952669f5030b127cf8e19c93dafc66e3f
root <root@onmetal-server-01.localdomain>
2015-06-19 17:44:30 +0000

    Changes to test script

--- test.sh
+++ test.sh
@@ -2,14 +2,17 @@
 
 ghc_prefix=/mnt/drv1/ghc-root
 
+PATH=/opt/ghc/7.8.3/bin:$PATH
+
 pushd ..
 ghc_rev=$(git rev-parse HEAD)
+rm -Rf libraries/bootstrapping.conf
 make -j40 distclean || true
 git submodule update
 ls
 pwd
 ./boot
-./configure --prefix=$ghc_prefix
+./configure --prefix=$ghc_prefix --with-ghc=/opt/ghc/7.8.3/bin/ghc
 make -j40
 make -j40 install
 popd
@@ -19,14 +22,19 @@ LD_LIBRARY_PATH=$ghc_prefix/lib:$LD_LIBRARY_PATH
 
 cabal sandbox delete || true
 cabal sandbox init
-cabal install hashable-1.2.3.2/
-cabal install accelerate-0.15.1.0/
-cabal install --only-dependencies
+rm -fR dist accelerate/dist hashable-1.2.3.2/dist
+cabal install accelerate/ hashable-1.2.3.2/ -j4 
+cabal install -j4 --only-dependencies
 
 res=0
-timeout 120 cabal build -j1 || res=$?
-if [ $res == 0 ]; then
+timeout --signal=KILL --foreground 7m cabal install -j1 > $ghc_rev.log 2>&1 || res=$?
+if [ $res == 124 ]; then
+	# Timeout means we've failed, but we're doing an inverted bisection
+	echo "$ghc_rev is bad"
 	exit 0
+elif [ $res == 0 ]; then
+	echo "$ghc_rev finished compiling successfully"
+	exit 1
 else
 	echo "Commit $ghc_rev failed with status $res"
 	exit 1
